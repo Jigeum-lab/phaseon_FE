b21ce0f605556858e3307ddff9ace7f0ff3a74dc
Seong Gi Dong <sgd0947@gmail.com>
2024-09-13 19:23:04 +0900

    refactor(eslint): extends 순서 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'airbnb',
     'airbnb/hooks',
     'airbnb-typescript',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
